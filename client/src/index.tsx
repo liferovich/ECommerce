c26332868578f2fd00567684ab80442a4881186a
liferovich <miss.oliferovich@mail.ru>
2022-05-14 16:22:54 +0300

    comments

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -5,7 +5,6 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux';
 import { store } from './app/store';
-// import './assets/css/style.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
